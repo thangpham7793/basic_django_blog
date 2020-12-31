fa301452b7881d77d99ea8867ffecb0278b48661
Thang <thangnus@gmail.com>
2020-12-31 22:09:28 +1300

    Fix setting bug and add Custom Manager for Published Posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "author", "published", "status")
+    list_filter = ("status", "created", "published", "author")
+    search_fields = ("author", "title")
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ("author",)
+    date_hierarchy = "published"
+    ordering = ("status", "published")
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        # calling the parent's get_queryset method
+        return super(PublishedManager, self).get_queryset().filter(status="published")
+
+
+class Post(models.Model):
+
+    # default model manager
+    objects = models.Manager()
+    # custom model manager
+    published_posts = PublishedManager()
+    # first is value set in db, second is human-readable
+
+    STATUS_CHOICES = (
+        ("draft", "DRAFT"),
+        ("published", "PUBLISHED"),
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="published")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    body = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+
+    class Meta:
+        ordering = ("-published",)
+
+    def __str__(self):
+        return self.title
